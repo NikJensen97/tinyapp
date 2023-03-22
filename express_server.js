c6a17bcc5c5098e401d95327b6580df0d06e38c7
NikJensen97 <npjjensen@hotmail.com>
2023-03-21 17:29:53 -0700

    delete button now deletes data from database

--- express_server.js
+++ express_server.js
@@ -44,6 +44,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newID}`);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  console.log(req.params.id);
+  delete urlDatabase[req.params.id];
+  res.redirect(`/urls`);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: `${urlDatabase[req.params.id]}` };
   res.render("urls_show", templateVars);
